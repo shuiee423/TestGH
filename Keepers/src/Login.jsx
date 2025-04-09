ac5992fba39a2fd9ebcbf0fd47b8bd0b50b007a3
shuiee423 <indino.samhenrie@gmail.com>
2025-04-09 23:04:52 +0800

    Small update
    hehe cute si Sam

--- Keepers/src/Login.jsx
+++ Keepers/src/Login.jsx
@@ -9,14 +9,14 @@ function Login() {
         <h2>Login</h2>
         <form>
           <input type="numbers" placeholder="School ID" required />
-          <input type="email" placeholder="Email Account" required />
+          <input type="email" placeholder="Email-Account" required />
           <button type="submit">Login</button>
 
           <div className="divider">
             <h1>or</h1>
           </div>
 
-          <div className="signup">
+          <div className="sign up">
             <h2>Don't have an account?</h2>
             <Link to="/signup">
               <button type="button">Sign Up</button>
